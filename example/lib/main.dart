5586d527364dc347fab186a99a282d0c06091cb4
王金东(17082054) <419591321@qq.com>
2023-05-16 10:28:14 +0800

    支持web canvaskit模式

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamilyFallback: ['Roboto'],
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
