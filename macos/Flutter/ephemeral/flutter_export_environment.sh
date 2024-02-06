c00bc266a14ab70182733d80b997c3caa1ca8517
JD <419591321@qq.com>
2024-02-06 15:24:36 +0800

    up

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/sn/fvm/versions/3.10.5"
+export "FLUTTER_ROOT=/Users/sn/fvm/versions/3.13.6"
 export "FLUTTER_APPLICATION_PATH=/Users/sn/Documents/Github/flutter/flutter_chart"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.1.4"
-export "FLUTTER_BUILD_NUMBER=0.1.4"
+export "FLUTTER_BUILD_NAME=0.2.5"
+export "FLUTTER_BUILD_NUMBER=0.2.5"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
